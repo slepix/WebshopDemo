924a1340f86935e8deaa218ebdb41eba21ab9a7d
Alesandro <124347018+aslepcev@users.noreply.github.com>
2025-09-16 11:15:56 +0200

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api-frontend.lke500822.akamai-apl.net/api';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api-frontend.lke516711.akamai-apl.net/api';
 
 export const config = {
   api: {
